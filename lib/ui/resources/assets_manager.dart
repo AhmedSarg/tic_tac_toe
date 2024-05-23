0f4c41388c4200a5a355b5953598799ce5e6ed1b
AhmedSarg <91041775+AhmedSarg@users.noreply.github.com>
2024-05-23 14:00:50 +0300

    finishing scoreboard

--- lib/ui/resources/assets_manager.dart
+++ lib/ui/resources/assets_manager.dart
@@ -2,4 +2,6 @@ class AssetsManager {
   static const String xLottie = 'assets/lotties/x_lottie.json';
   static const String oLottie = 'assets/lotties/o_lottie.json';
   static const String bgLight = 'assets/images/bg_light.jpg';
+  static const String botIcon = 'assets/icons/bot_icon.svg';
+  static const String personIcon = 'assets/icons/person_icon.svg';
 }
